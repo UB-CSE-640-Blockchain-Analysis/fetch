109a58e03e0661acdf5e9fa4645ff1dad3c02306
joelpatel <joelpatel.cs@gmail.com>
2022-03-24 21:12:38 -0400

    add connetion apis to store data in neo4j graph database

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import requests
 import json
 import datetime
+from neo4j import GraphDatabase
+
+"""
+    database connection and inserting
+"""
+
+
+def execute_transactions(transaction_execution_commands):
+    # connect to database
+    database_connection = GraphDatabase.driver(
+        uri="bolt://localhost:7687", auth=("neo4j", "640"))
+    session = database_connection.session()
+    # "insert" (in neo4j terms, create) each transaction into DB
+    for tx in transaction_execution_commands:
+        i = "create (a1: Address{addr:\"" + str(tx["sender"]) + "\"}), (a2: Address{addr:\"" + str(tx["receiver"]) + "\"}), (a1) -[:SENDS{timestamp: \"" + str(
+            tx["time"]) + "\", amount: \""+str(tx["value"])+"\", hash: \"" + str(tx["hash"]) + "\", source: \"" + str(tx["sender"]) + "\", target: \"" + str(tx["receiver"]) + "\"}]-> (a2)"
+        session.run(i)
 
 
 global main_hash
 main_hash = {}
 global second_hash
 second_hash = {}
+# transactions = []
+database_connection = GraphDatabase.driver(
+    uri="bolt://localhost:7687", auth=("neo4j", "640"))
+session = database_connection.session()
 
 
 def handle_input(inputs):
@@ -79,8 +100,8 @@ def handle_output(outputs, input):
                     if len(final_outputs) > 0:  # TODO verify correctness
                         for el_index in range(len(final_outputs)):
                             if final_outputs[el_index]["addr"] == addr:
-                                final_outputs[el_index]["value"] = int(
-                                    final_outputs[el_index]["value"]) + int(output["value"])
+                                final_outputs[el_index]["value"] = int(float(
+                                    final_outputs[el_index]["value"])) + int(float(output["value"]))
                                 break
                             else:
                                 final_outputs.append(
@@ -90,7 +111,7 @@ def handle_output(outputs, input):
                             {"value": str(output["value"]), "addr": str(addr)})
             else:
                 final_outputs.append(
-                    {"value": str(int(output["value"])/100000000), "addr": str(output["addr"])})
+                    {"value": str(int(float(output["value"]))/100000000), "addr": str(output["addr"])})
 
     return (final_outputs)
 
@@ -101,6 +122,8 @@ today = datetime.datetime.now().date()
 
 sum_hash = 0
 sum_transactions = 0
+
+print("fetchin data")
 for day in range(pastNDays):
     particular_day = int((datetime.datetime(
         today.year, today.month, today.day).timestamp() - (86400.0 * day)) * 1000)
@@ -114,7 +137,7 @@ for day in range(pastNDays):
         single_block_res = (requests.get(single_block)).json()
         sum_transactions += (len(single_block_res["tx"]))
         for each_transaction in single_block_res["tx"]:
-            print("tx: " + str(each_transaction["hash"]))  # log
+            # print("tx: " + str(each_transaction["hash"]))  # log
             # parse through each transaction as we already have the transaction data fetched
             inputs = []
             outputs = []
@@ -157,10 +180,18 @@ for day in range(pastNDays):
                     # dict = {"hash": each_transaction["hash"],"time": each_transaction["time"], "sender":inputs, "receivers":outputs}
                     # if each_transaction["hash"] == "293a5e6c0eea9ed493c5982f638b15b9d669d1c5304424024f7c1a04f605b429":
 
-                        print(json.dumps(dict))
-                        print("\n--------\n")
-                    # TODO append to the main dataset
+                        # print(json.dumps(dict))
+                        # print("\n--------\n")
+                        # TODO append to the main dataset
+                        i = "merge (a1: Address{addr:\"" + str(dict["sender"]) + "\"})\n merge (a2: Address{addr:\"" + str(dict["receiver"]) + "\"})\n merge (a1) -[:SENDS{timestamp: \"" + str(
+                            dict["time"]) + "\", amount: \""+str(dict["value"])+"\", hash: \"" + str(dict["hash"]) + "\", source: \"" + str(dict["sender"]) + "\", target: \"" + str(dict["receiver"]) + "\"}]-> (a2)"
+                        session.run(i)
 
+print("done fetching data")
+# with open('transactions.json', 'w') as outfile:
+#     json.dump(transactions, outfile)
+# execute_transactions(transactions)
+print("done storing data")
 print(sum_hash)
 
 # print(datetime.datetime(2022, 3, 1).timestamp())
