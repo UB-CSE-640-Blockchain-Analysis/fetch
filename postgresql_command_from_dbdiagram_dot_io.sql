44ee4e7731f8956b64ab8ce4858005d2ab64e8b3
joelpatel <joelpatel.cs@gmail.com>
2022-04-24 00:41:19 -0400

    add command to create history table

--- postgresql_command_from_dbdiagram_dot_io.sql
+++ postgresql_command_from_dbdiagram_dot_io.sql
@@ -15,7 +15,9 @@ CREATE INDEX ON "secondary_hash" ("secondary_address");
 CREATE TABLE "history" (
   "particular_day_seconds" bigint,
   "block_index" bigint,
-  "transaction_index" bigint
+  "transaction_index" bigint,
+  "starting_date" date
 );
 -- INSERT INTO history VALUES (1650513600000, 3, 2);
+-- INSERT INTO history VALUES (0, 0, 0, null);
 -- UPDATE history SET (particular_day_seconds, block_index, transaction_index) = (2650513600000, 3, 2) where particular_day_seconds = 1650513600000;
